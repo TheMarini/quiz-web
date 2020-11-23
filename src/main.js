cdcb17bbe5e77115e0a6d8d06f7a478d4ebf17e6
Bruno Marini <bruno.marini1@gmail.com>
2020-11-22 21:13:48 -0300

    [+] Package: NES.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+// --- IMPORTS ---
+// Third party
+import 'nes.css/css/nes.min.css';
+
+// Essentials
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// --- CONFIGS ---
+// Prevent the production tip on Vue startup
 Vue.config.productionTip = false;
 
+// --- INITIALIZATION ---
 new Vue({
   router,
   store,
