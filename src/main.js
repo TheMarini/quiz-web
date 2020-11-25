63c0d1f156200bdf924deeb66197f20ae471d16a
Bruno Marini <bruno.marini1@gmail.com>
2020-11-25 19:27:54 -0300

    [+] Packages: Bootstrap Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // --- IMPORTS ---
 // Third party
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'nes.css/css/nes.min.css';
 
 // Essentials
@@ -7,6 +10,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+// --- USES ---
+Vue.use(BootstrapVue);
 
 // --- CONFIGS ---
 // Prevent the production tip on Vue startup
