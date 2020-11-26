fb458981f6da865809ce64549cae366887192d19
Bruno Marini <bruno.marini1@gmail.com>
2020-11-25 21:04:39 -0300

    [^] Style: text color, link

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,23 @@
   font-family: 'Press Start 2P', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #222;
+}
+
+a,
+button {
+  cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+      14 0,
+    pointer !important;
+}
+
+a {
+  text-decoration: underline;
+  /* transition: 0.3s; */
+}
+
+a:hover {
+  /* background-color: #222; */
+  /* color: #fff !important; */
 }
 </style>
