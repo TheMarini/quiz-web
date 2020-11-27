ddae1503ffc82457dbff02b89c0c5c30faa93721
Bruno Marini <bruno.marini1@gmail.com>
2020-11-27 20:46:41 -0300

    [^] Register: disable button by inputs length

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,23 +4,44 @@
       <h3 class="mb-4">Quiz APP</h3>
       <form class="text-left">
         <div class="nes-field form-group">
-          <label for="name">Nome Completo</label>
-          <input id="name" type="text" class="nes-input" placeholder="Kagemasa Kozuki" />
+          <label for="name" class="nes-text" :class="nameClass">Nome Completo</label>
+          <input
+            id="name"
+            v-model="name"
+            type="text"
+            class="nes-input"
+            :class="nameClass"
+            placeholder="Kagemasa Kozuki"
+          />
         </div>
         <div class="nes-field form-group">
           <label for="login">Login</label>
-          <input id="login" type="text" class="nes-input" placeholder="konami" />
+          <input id="login" v-model="login" type="text" class="nes-input" placeholder="konami" />
         </div>
         <div class="nes-field form-group">
           <label for="pwd">Senha</label>
-          <input id="pwd" type="password" class="nes-input" placeholder="*****" />
+          <input id="pwd" v-model="pwd" type="password" class="nes-input" placeholder="*****" />
         </div>
         <div class="nes-field form-group">
           <label for="pwd-repeat">Confirmar senha</label>
-          <input id="pwd-repeat" type="password" class="nes-input" placeholder="*****" />
+          <input
+            id="pwd-repeat"
+            v-model="pwdRepeat"
+            type="password"
+            class="nes-input"
+            placeholder="*****"
+          />
         </div>
         <div class="d-flex">
-          <button type="button" class="nes-btn is-success btn-block">Registrar</button>
+          <button
+            type="button"
+            class="nes-btn btn-block"
+            :disabled="btnDisabled"
+            :class="btnClass"
+            @click="register"
+          >
+            Registrar
+          </button>
         </div>
         <hr />
         <div class="register text-center">
@@ -34,6 +55,31 @@
 <script>
 export default {
   name: 'Login',
+  data() {
+    return {
+      name: '',
+      login: '',
+      pwd: '',
+      pwdRepeat: '',
+      btnClicked: false,
+    };
+  },
+  computed: {
+    btnDisabled() {
+      return (
+        this.name.length === 0 ||
+        this.login.length === 0 ||
+        this.pwd.length === 0 ||
+        this.pwdRepeat.length === 0
+      );
+    },
+    btnClass() {
+      return this.btnDisabled ? 'is-disabled' : 'is-success';
+    },
+  },
+  methods: {
+    register() {},
+  },
 };
 </script>
 
