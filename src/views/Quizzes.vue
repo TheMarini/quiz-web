91ad749c0f6d163c23ca9e3fcd8463a7577bdde0
Bruno Marini <bruno.marini1@gmail.com>
2020-11-28 03:13:07 -0300

    [^] Quizzes: responsivity, create button

--- src/views/Quizzes.vue
+++ src/views/Quizzes.vue
@@ -2,13 +2,24 @@
   <div class="quizzes container py-4">
     <header>
       <h1 class="title text-center mb-4">Quiz APP</h1>
-      <div class="mb-4 text-white d-flex justify-content-between">
-        <p>Bem vindo <span>Bruno Marini</span> :)</p>
-        <router-link to="/" class="text-reset logout">Sair</router-link>
+      <div class="mb-4 text-white row justify-content-between">
+        <div class="col-sm-12 col-md-auto">
+          <p>Bem vindo <span>Bruno Marini</span> :)</p>
+        </div>
+        <div class="col-sm-12 col-md-auto">
+          <router-link to="/" class="text-reset logout">Sair</router-link>
+        </div>
       </div>
     </header>
     <article>
-      <h4 class="title mb-4">Meus quizzes</h4>
+      <div class="mb-4 text-white row justify-content-between">
+        <div class="col-sm-12 col-md-auto">
+          <h4 class="title mb-4">Meus quizzes</h4>
+        </div>
+        <div class="col-sm-12 col-md-auto">
+          <button type="button" class="m-0 nes-btn is-success">Criar quiz</button>
+        </div>
+      </div>
       <div class="row">
         <div class="col-sm-12 col-md-6">
           <Quiz></Quiz>
